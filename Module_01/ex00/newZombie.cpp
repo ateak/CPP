7d5948a0316b8ec0f65d54657a8fa560a228e1af
Antwan Teak <ateak@at-h4.msk.21-school.ru>
2022-11-14 21:15:42 +0300

    newZombie.cpp upd

--- Module_01/ex00/newZombie.cpp
+++ Module_01/ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: ateak <ateak@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 14:49:11 by ateak             #+#    #+#             */
-/*   Updated: 2022/11/14 14:49:12 by ateak            ###   ########.fr       */
+/*   Updated: 2022/11/14 21:15:09 by ateak            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,9 @@
 
 Zombie *newZombie(std::string name)
 {
-<<<<<<< HEAD
 	Zombie *dead = new (std::nothrow)Zombie(name); // std::nothrow возвращает нулевой указатель, если память не выделится,
 												// вместо того, чтобы выдать exception
 	if (!dead)
 		std::cout << "Could not allocate memory!" << std::endl;
-=======
-	Zombie *dead = new(std::nothrow)Zombie(name); // указатель dead станет нулевым, если память не выделится 
-							// (std::nothrow для того, чтобы избежать генерации исключения)
-	if (!dead)
-	    std::cout << "Could not allocate memory!" << std::endl;
->>>>>>> b363ac38f336c442d7a1d78b579b7dfcc4062b21
 	return (dead);
 }
