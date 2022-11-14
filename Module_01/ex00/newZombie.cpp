b363ac38f336c442d7a1d78b579b7dfcc4062b21
Ekaterina Timoshkina <until_the_end@inbox.ru>
2022-11-14 14:47:32 +0300

    Update newZombie.cpp

--- Module_01/ex00/newZombie.cpp
+++ Module_01/ex00/newZombie.cpp
@@ -17,8 +17,6 @@ Zombie	*newZombie(std::string name)
 	Zombie *dead = new(std::nothrow)Zombie(name); // указатель dead станет нулевым, если память не выделится 
 							// (std::nothrow для того, чтобы избежать генерации исключения)
 	if (!dead)
-	{
-		std::cout << "Could not allocate memory!" << std::endl;
-	}
+	    std::cout << "Could not allocate memory!" << std::endl;
 	return (dead);
 }
