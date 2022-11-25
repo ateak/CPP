519b6158bb0524fcab3c470c7da225eb41981569
Antwan Teak <ateak@at-h4.msk.21-school.ru>
2022-11-25 14:58:07 +0300

    upd ex05

--- Module_01/ex05/main.cpp
+++ Module_01/ex05/main.cpp
@@ -1,14 +1,8 @@
 #include "Harl.hpp"
 
-int main(int argc, char **argv)
+int main()
 {
 	Harl harl;
-
-	if (argc != 2)
-	{
-		std::cout << "Wrong number of arguments" << std::endl;
-		return (1);
-	}
 
 	std::cout << "Correct requests:" << std::endl;
 	harl.m_complain("DEBUG");
